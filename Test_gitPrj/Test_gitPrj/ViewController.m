dc34f80502425a58cb1eadb8a9fcd38e2e1da973
wuxiaochuang <xiaochuang0120@163.com>
2016-02-24 14:53:13 +0800

    yanse

--- Test_gitPrj/Test_gitPrj/ViewController.m
+++ Test_gitPrj/Test_gitPrj/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     self.title = @"github 代码修改";
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 - (void)didReceiveMemoryWarning {
